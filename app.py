56d0a5b7afaf5d1f334535edf98b351599131c3d
Bryce Beckenbach <brycebeckenbach@gmail.com>
2022-05-26 12:40:38 -0400

    added class file for S3Manager

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import boto
-import os
+
 import pandas as pd
 import yfinance as yf
 '''
---
+++ classes/S3Manager.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+
+class S3Manager:
+    '''Class to set up connection with AWS S3 Data Lake'''
+    def __init__(self) -> None:
+        self.aws_key_id = os.getenv('AWS_KEY_ID')
+        self.aws_secret_key = os.getenv('AWS_SECRET_KEY')
+        self.region = 'us-east-1'
+        self.S3Client = self.create_S3_client()
+
+    def create_S3_client(self):
+        s3_client = boto3.client('s3',
+                                aws_access_key=self.aws_key_id,
+                                aws_secret_access_key=self.aws_secret_key,
+                                region=self.region)
+        return s3_client
