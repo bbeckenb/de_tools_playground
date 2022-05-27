10b9552529bf5a03073eec788a7c31067ca2ccab
Bryce Beckenbach <brycebeckenbach@gmail.com>
2022-05-27 13:36:05 -0400

    most up to date resolved unresolved modules

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from classes.S3Manager import S3Manager
-from classes.YahooFiManager import YahooFiManager
+
 '''
 I think a mini-project here would be a great way to go about learning these tools. A potential project you could do:
 
-1. Set up airflow instance to run locally
+1. Set up airflow instance to run locally - DONE
     - Directed Acyclic Graphs (DAGs) of tasks
-2. Create personal AWS account (the rest of this should be within the free tier of usage)
+2. Create personal AWS account (the rest of this should be within the free tier of usage) - DONE
 3. Create airflow job to:
     - Pull structured data from an online data source (stock data is pretty easy to get, could also do sports or travel information)
     - Write the data to an S3 bucket
@@ -15,5 +14,5 @@ I think a mini-project here would be a great way to go about learning these tool
     - Send the data to yourself in an email with the details in an excel attachment to the file
 '''
 
-S3Client = S3Manager()
-YahooClient = YahooFiManager()
+
+
--- dags/send_stock_data_to_S3.py
+++ dags/send_stock_data_to_S3.py
@@ -1,8 +1,11 @@
-import csv
 import datetime
 from io import StringIO
 import pandas as pd
 from common.clients import YahooClient, AwsClient
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
 
 def get_stock_data(tickers) -> pd.DataFrame:
     today = datetime.datetime.today()
@@ -20,5 +23,18 @@ def write_to_s3(data: pd.DataFrame) -> None:
     AwsClient.s3_res.Object(bucket_name, s3_obj_name).put(Body=csv_buffer.getvalue())
     print('success!')
 
-data = get_stock_data('AMZN')
-print(write_to_s3(data))
+def send_stock_data_to_S3():
+    data = get_stock_data('AMZN')
+    write_to_s3(data)
+
+with DAG(dag_id='send_stock_data_to_S3',
+        start_date=datetime.datetime(2022,5,27),
+        schedule="@hourly",
+        catchup=False) as dag:
+
+    task1 = PythonOperator(task_id='send_stock_data_to_S3',
+                            python_callable=send_stock_data_to_S3)
+
+task1
+
+
