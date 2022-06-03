1787095590a058526054a641e9ef26159af6e4fd
Bryce Beckenbach <brycebeckenbach@gmail.com>
2022-06-03 14:16:49 -0400

    updated file structure for dags and tasks

---
+++ dags/stock_dag.py
@@ -0,0 +1,16 @@
+from airflow.operators.python_operator import PythonOperator
+from airflow import DAG
+import datetime
+from tasks.send_stock_data_to_S3 import send_stock_data_to_S3
+
+
+
+with DAG(dag_id='stock_dag',
+        start_date=datetime.datetime(2022,6,2),
+        schedule_interval='@daily',
+        catchup=False) as dag:
+
+    task1 = PythonOperator(task_id='send_stock_data_to_S3',
+                            python_callable=send_stock_data_to_S3)
+
+task1
--- dags/tasks/send_stock_data_to_S3.py
+++ dags/tasks/send_stock_data_to_S3.py
@@ -2,10 +2,6 @@ import datetime
 from io import StringIO
 import pandas as pd
 from common.clients import YahooClient, AwsClient
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-
-
 
 def get_stock_data(tickers) -> pd.DataFrame:
     today = datetime.datetime.today()
@@ -19,7 +15,8 @@ def write_to_s3(data: pd.DataFrame) -> None:
     csv_buffer = StringIO()
     data.to_csv(csv_buffer)
     bucket_name = 'brycepracticebucket'
-    s3_obj_name = f'amzn/{datetime.datetime.today()}.csv'
+    today_date = datetime.datetime.today().strftime('%Y-%m-%d')
+    s3_obj_name = f'amzn/{today_date}.csv'
     AwsClient.s3_res.Object(bucket_name, s3_obj_name).put(Body=csv_buffer.getvalue())
     print('success')
 
@@ -27,16 +24,5 @@ def send_stock_data_to_S3():
     data = get_stock_data('AMZN')
     write_to_s3(data)
 
-with DAG(dag_id='send_stock_data_to_s3',
-        start_date=datetime.datetime(2022,6,2),
-        schedule_interval='@daily',
-        catchup=False) as dag:
-
-    task1 = PythonOperator(task_id='send_stock_data_to_S3',
-                            python_callable=send_stock_data_to_S3)
-
-task1
-
-# send_stock_data_to_S3()
 
 
